dd23ee5fce6823b060cb2811ed94c1e0e70124ae
Sarwesh Suman <sarwesh.suman@ARICENT.COM>
2017-09-28 23:27:50 +0530

    NaiveBayes

--- classification/NaiveBayes.py
+++ classification/NaiveBayes.py
@@ -44,7 +44,13 @@ X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.2)
 # 3. MultinomialNB
 #   Best for making discrete class predictions
 
-model = GaussianNB()
+# Any of these model can be selected
+
+#model = GaussianNB()
+#model = MultinomialNB()
+model = BernoulliNB()
+
+
 model.fit(X_train,Y_train)
 
 # prediction
